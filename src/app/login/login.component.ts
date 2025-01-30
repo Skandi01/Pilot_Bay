0f106398d9077d1b56706f3cc934e71650c3423f
Skandi01 <igor.tirex@yandex.ru>
2025-01-30 09:54:02 +0400

    наращиваем компоненты

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  template: `<p>login works!</p>`,
+  styleUrl: './login.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent { }
