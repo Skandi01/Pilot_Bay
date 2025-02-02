bf1a605ca2403a043c0346fe63d8587307fe3a8b
Skandi01 <igor.tirex@yandex.ru>
2025-02-02 23:34:16 +0400

    радиокнопки регистрации

--- src/app/authorise/authorise.component.ts
+++ src/app/authorise/authorise.component.ts
@@ -1,11 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { AuthPilotComponent } from './auth-pilot/auth-pilot.component';
+import { AuthAirfieldComponent } from './auth-airfield/auth-airfield.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-authorise',
   standalone: true,
-  imports: [],
-  template: `<p>authorise works!</p>`,
+  imports: [AuthPilotComponent, AuthAirfieldComponent, FormsModule, NgIf],
+  templateUrl: './authorise.component.html',
   styleUrl: './authorise.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AuthoriseComponent { }
+export class AuthoriseComponent {
+  radio: boolean = true;
+  //radio2: boolean = false;
+  
+ }
