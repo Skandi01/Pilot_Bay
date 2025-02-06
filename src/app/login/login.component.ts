f0334ec1e12a205d8f3af38bb2a775e3b7e6c0a2
Skandi01 <57838723+Skandi01@users.noreply.github.com>
2025-02-06 22:21:20 +0400

    валидация формы логина, внедрение app.module

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,24 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {FormGroup, FormControl, Validators } from '@angular/forms';
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent { }
+export class LoginComponent { 
+  
+  loginForm = new FormGroup({    
+    email: new FormControl('',[Validators.required,Validators.email,Validators.nullValidator]),    
+    password: new FormControl('',[Validators.required,Validators.nullValidator]),
+    remindme: new FormControl('')
+  });
+
+  onSubmit(form: any){
+    console.warn(this.loginForm.value);
+  }
+}
