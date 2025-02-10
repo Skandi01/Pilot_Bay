e040178682747bdeadcd0ad6f29de94b6f997d0d
Skandi01 <57838723+Skandi01@users.noreply.github.com>
2025-02-10 06:55:38 +0400

    доп коммит

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {IUser} from '../../models/IUser';
 
@@ -12,11 +13,17 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  createUser(): Observable<IUser> {
-    return this.http.get<IUser>(`${this.apiUrl}/users/create_user`)
+  createUser(user: IUser): Observable<IUser> {
+    return this.http.post<IUser>(
+      this.apiUrl, 
+      user, 
+      {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+      }
+    );
   }
 
-  getUsers(): Observable<IUser[]>  {
-    return this.http.get<IUser[]>(`${this.apiUrl}/users/get_users`);
+  getUsers(): Observable<IUser[]> {
+    return this.http.get<IUser[]>(`${this.apiUrl}/user/getAll`);
   }
 }
